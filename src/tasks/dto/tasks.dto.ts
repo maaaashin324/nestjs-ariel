c3df8aabe8c23633082d3325f6d39c2110f80c28
maaaashin324 <masataka.shintoku@gmail.com>
2024-09-05 07:23:54 +0000

    feat: adding validation to task dto

--- src/tasks/dto/tasks.dto.ts
+++ src/tasks/dto/tasks.dto.ts
@@ -1,7 +1,10 @@
+import { IsNotEmpty } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class CreateTaskDto {
+  @IsNotEmpty()
   title: string;
+  @IsNotEmpty()
   description: string;
 
   constructor(title: string, description: string) {
