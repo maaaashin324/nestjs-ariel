10c57e10d81ea9c0f23b9724d9b0075b3d820950
maaaashin324 <masataka.shintoku@gmail.com>
2024-09-06 14:51:39 +0000

    feat: using postgres on nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.local',
+    }),
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      // entity settings will be automated
+      autoLoadEntities: true,
+      // schema synchronization will be automated
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
