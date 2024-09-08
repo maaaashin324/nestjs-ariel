d08bc62c8b6270cfa5b5a246c464f2ae1c23d4f9
maaaashin324 <masataka.shintoku@gmail.com>
2024-09-08 06:10:11 +0000

    feat: adding user repository and create an endpoint named sign-up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './user.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userRepository: UsersRepository) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.createUser(authCredentialsDto);
+  }
+}
