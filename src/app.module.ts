0cda28b4c9251ef121e6b4244f8f8610e18b0088
maaaashin324 <masataka.shintoku@gmail.com>
2024-09-08 05:32:33 +0000

    feat: adding auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { ConfigModule } from '@nestjs/config';
       // schema synchronization will be automated
       synchronize: true,
     }),
+    AuthModule,
   ],
 })
 export class AppModule {}
