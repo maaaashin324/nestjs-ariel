876e6dc1f8834a6fd7d583e54160b7efc111aff0
maaaashin324 <masataka.shintoku@gmail.com>
2024-09-08 08:38:26 +0000

    feat: adding GetUser decorator

--- src/auth/get-user.decorator.ts
+++ src/auth/get-user.decorator.ts
@@ -2,7 +2,7 @@ import { createParamDecorator } from '@nestjs/common';
 import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
 import { User } from './user.entity';
 
-export const getUser = createParamDecorator(
+export const GetUser = createParamDecorator(
   (_, ctx: ExecutionContextHost): User => {
     const req = ctx.switchToHttp().getRequest();
 
