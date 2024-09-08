c337ade0e930f254bf4aa21b828e675e378ca6e1
maaaashin324 <masataka.shintoku@gmail.com>
2024-09-08 06:56:23 +0000

    feat: adding unique validation for username

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,11 @@
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { IUserRepository } from './auth.service';
 import { InjectRepository } from '@nestjs/typeorm';
 
@@ -20,6 +24,13 @@ export class UsersRepository implements IUserRepository {
       password,
     });
 
-    await this.usersRepository.save(user);
+    try {
+      await this.usersRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      }
+      throw new InternalServerErrorException(error);
+    }
   }
 }
